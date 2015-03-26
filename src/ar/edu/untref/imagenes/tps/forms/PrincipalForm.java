2a152013b1b41de0ced3e14421336125390e6727
ebeneventano <emmanuel.beneventano@gmail.com>
2015-03-26 10:36:27 -0300

    Nuevos menus y abrir imagen

--- src/ar/edu/untref/imagenes/tps/forms/PrincipalForm.java
+++ src/ar/edu/untref/imagenes/tps/forms/PrincipalForm.java
@@ -1,19 +1,41 @@
 package ar.edu.untref.imagenes.tps.forms;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 @SuppressWarnings("serial")
 public class PrincipalForm extends JFrame{
+	
 	private JMenuBar menuBar;
-	private JMenu menu;
+	private JMenu menuArchivo;
+	private JMenu menuHistograma;
+	private JMenuItem menuOpenImage;
+	private JMenuItem menuSaveAs;
+	private JMenuItem menuRefreshChanges;
+
+	private JScrollPane scrollPane;
 	private JPanel contentPane;
+	
+	private JLabel labelPrincipalImage;
+	
+	private BufferedImage imageInLabel;
+	private BufferedImage originalImage;
+
 	
 	public PrincipalForm (){
 
@@ -21,14 +43,18 @@ public class PrincipalForm extends JFrame{
 		setBounds(100, 100, 800, 600);
 		
 		this.initializeMenu();
+		this.addListenersToComponents();
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		labelPrincipalImage = new JLabel();
+		scrollPane.setViewportView(labelPrincipalImage);
 	}
 
 	private void initializeMenu() {
@@ -36,10 +62,67 @@ public class PrincipalForm extends JFrame{
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		menu = new JMenu("Archivo");
-		menuBar.add(menu);
+		menuArchivo = new JMenu("Archivo");
+		menuBar.add(menuArchivo);
+		
+		menuOpenImage = new JMenuItem("Abrir Imagen");
+		menuArchivo.add(menuOpenImage);
+		
+		menuSaveAs = new JMenuItem("Guardar Como");
+		menuArchivo.add(menuSaveAs);
+		
+		menuRefreshChanges = new JMenuItem("Deshacer Cambios");
+		menuArchivo.add(menuRefreshChanges);
+		
+		menuHistograma = new JMenu("Histograma");
+		menuBar.add(menuHistograma);
 	}
 	
+	private void addListenersToComponents() {
+		
+		menuOpenImage.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				labelPrincipalImage.setIcon(new ImageIcon(abrirImagen()));
+			}
+		});
+		
+		menuRefreshChanges.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				refreshChanges();
+			}
+		});
+		
+	}
+	
+	public BufferedImage abrirImagen() {
+
+		JFileChooser selector = new JFileChooser();
+		selector.setDialogTitle("Seleccione una imagen");
+
+		int flag = selector.showOpenDialog(null);
+
+		if (flag == JFileChooser.APPROVE_OPTION) {
+			try {
+
+				File archivoSeleccionado = selector.getSelectedFile();
+				imageInLabel = ImageIO.read(archivoSeleccionado);
+				originalImage = ImageIO.read(archivoSeleccionado);
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		return imageInLabel;
+	}
+    
+    private void refreshChanges(){
+		labelPrincipalImage.setIcon(new ImageIcon(originalImage));
+    }
+
 	
 
 }
