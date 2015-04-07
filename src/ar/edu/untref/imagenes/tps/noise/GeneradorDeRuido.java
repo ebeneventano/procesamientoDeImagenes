a352c121655a9c45d489bedefddaafd0f35ad7a0
Facundo Rodrigue Arceri <frodriguez.arceri@gmail.com>
2015-04-07 19:29:14 -0300

    Punto 8.A), generador de números aleatorios Gaussianos

--- src/ar/edu/untref/imagenes/tps/noise/GeneradorDeRuido.java
+++ src/ar/edu/untref/imagenes/tps/noise/GeneradorDeRuido.java
@@ -1,10 +1,21 @@
 package ar.edu.untref.imagenes.tps.noise;
 
+import java.util.Random;
+
 public class GeneradorDeRuido {
 
 	public double generarNumeroAleatorioGaussiano(double desviacionEstandar, double valorMedio) {
 		
-		return 0;
+		double varianza = desviacionEstandar * desviacionEstandar;
+		double x = valorMedio + new Random().nextGaussian() * varianza;
+		
+		double coeficiente = 1 / ( desviacionEstandar * Math.sqrt( 2 * Math.PI ));
+		
+		double numeradorDeLaPotencia = (((-1) * x ) - valorMedio);
+		numeradorDeLaPotencia = numeradorDeLaPotencia * numeradorDeLaPotencia;
+		double denominadorDeLaPotencia = 2 * desviacionEstandar * desviacionEstandar;
+		
+		return coeficiente * Math.exp(numeradorDeLaPotencia / denominadorDeLaPotencia);
 	}
 	
 	public double generarNumeroAleatorioRayleigh(double epsilon) {
